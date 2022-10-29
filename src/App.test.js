711e95f5f538d5fe98c11667159ae9417fd2dfc9
Marshal Sahu <marshal.x.sahu@cssregtech.com>
2022-10-29 13:26:10 +0530

    initial commit

--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,15 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/was/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test('renders learn react link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/changed/i);
+  expect(linkElement).toBeInTheDocument();
+}); 
+
+
+
